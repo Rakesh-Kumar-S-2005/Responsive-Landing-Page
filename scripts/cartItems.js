60a59eb68df9531390c6c981942735daa1bce81b
Raghul Balaji <raghul19april@gmail.com>
2024-01-17 20:24:12 +0530

    hello

---
+++ scripts/cartItems.js
@@ -0,0 +1,63 @@
+export let productInfo=[
+    {
+        id:'15bcdefgh123-12db',
+        imageLink:'https://supersimple.dev/projects/amazon/images/products/athletic-cotton-socks-6-pairs.jpg',
+        imageDescription:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        ratings: {
+            starImageLink:'https://supersimple.dev/projects/amazon/images/ratings/rating-45.png',
+            noOfRatings:87
+        },
+        price:1090
+    },
+    {
+        id:'15bcdefgh123-12dc',
+        imageLink:'	https://supersimple.dev/projects/amazon/images/products/intermediate-composite-basketball.jpg',
+        imageDescription:'Intermediate Size Basketball',
+        ratings: {
+            starImageLink:'https://supersimple.dev/projects/amazon/images/ratings/rating-4.png',
+            noOfRatings:127
+        },
+        price:2095 //  1 dollar = 100 cents
+
+    },
+    {
+        id:'15bcdefgh123-12dd',
+        imageLink:'https://supersimple.dev/projects/amazon/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        imageDescription:'Adults Plain Cotton T-Shirt - 2 Pack',
+        ratings: {
+            starImageLink:'https://supersimple.dev/projects/amazon/images/ratings/rating-45.png',
+            noOfRatings:56
+        },
+        price:799
+    },
+    {
+        id:'15bcdefgh123-12de',
+        imageLink:'https://supersimple.dev/projects/amazon/images/products/black-2-slot-toaster.jpg',
+        imageDescription:'2 Slot Toaster - Black',
+        ratings: {
+            starImageLink:'https://supersimple.dev/projects/amazon/images/ratings/rating-5.png',
+            noOfRatings:2197
+        },
+        price:1899
+    },
+    {
+        id:'15bcdefgh123-12df',
+        imageLink:'https://supersimple.dev/projects/amazon/images/products/6-piece-white-dinner-plate-set.jpg',
+        imageDescription:'6 Piece White Dinner Plate Set',
+        ratings: {
+            starImageLink:'https://supersimple.dev/projects/amazon/images/ratings/rating-4.png',
+            noOfRatings:37
+        },
+        price:2067    
+    },
+    {
+        id:'15bcdefgh123-12dg',
+        imageLink:'https://supersimple.dev/projects/amazon/images/products/6-piece-non-stick-baking-set.webp',
+        imageDescription:'6-Piece Nonstick, Carbon Steel Oven',
+        ratings: {
+            starImageLink:'https://supersimple.dev/projects/amazon/images/ratings/rating-45.png',
+            noOfRatings:175
+        },
+        price:3499  
+    }
+];
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,1 +1,59 @@
- 
+import {cartList} from '../scripts/cart.js';
+import {productInfo} from '../scripts/cartItems.js';
+let concatHtmml1=' ';
+productInfo.forEach((cartproducts,index) => {
+    cartList.forEach((cartListItems,index) => {
+        if(cartListItems.cartid===cartproducts.id) {
+
+            let htmlElements1=`
+            <div class="cart info1">
+                <div class="delivery-date">Delivery date: Wednesday, January 24</div>
+                <div class="cart-item-details">
+                    <div class="image-container">
+                        <img class="cart-image" src="${cartproducts.imageLink}">
+                    </div>
+                    <div class="cart-description">
+                        <div class="name">${cartproducts.imageDescription}</div>
+                        <div class="cart-amount">${(cartproducts.price)/100}</div>
+                        <div class="cart-quantity">
+                            <div class="quantity">Quantity: ${cartListItems.quantity}</div>
+                            <div class="update">Update</div>
+                            <div class="delete">Delete</div>
+                        </div>
+                    </div>
+                    <div class="cart-delivery">
+                        <div class="delivery-option">Choose a delivery option:</div>
+                        <div class="radio">
+                            <input class="radio1" type="radio" name="options">
+                            <div class="options">
+                                <div class="date-option">Wednesday, January 24</div>
+                                <div class="delivery-type">FREE Shipping</div>
+                            </div>
+                        </div>
+                        <div class="radio">
+                            <input class="radio1" type="radio" name="options">
+                            <div class="options">
+                                <div class="date-option">Thursday, January 18</div>
+                                <div class="delivery-type">$4.99 - Shipping</div>
+                            </div>
+                        </div>
+                        <div class="radio">
+                            <input class="radio1" type="radio" name="options">
+                            <div class="options">
+                                <div class="date-option">Tuesday, January 16</div>
+                                <div class="delivery-type">$9.99 - Shipping</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        `;
+        }
+        concatHtmml1+=htmlElements1;
+        document.querySelector('.left-section').innerHTML=concatHtmml1;
+
+        
+    })
+        
+})
